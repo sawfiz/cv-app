4d61598677a2bbbb9a2b87fae2a675aa6a8e65ec
sawfiz <dashu.up@gmail.com>
2023-05-06 20:43:56 +0800

    Show first/last name when navigate back to personal info

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,29 +1,42 @@
 import { useState } from 'react';
 
 export default function PersonalInfo({ cv, updateCV }) {
+
+  // ^ The following does not work
+  // You are essentially setting state in your component during render which you should 
+  // never do. 
+  // When you call `setState(…)` you are telling your component to update and re-render. 
+  // And every time it re-renders it makes that `setState` call, causing it to infinitely loop.
   // const [info, setInfo] = useState({ firstName: '', lastName: '' });
   // if (cv.personalInfo) {
   //   const { firstName, lastName } = cv.personalInfo;
-  //   console.log("🚀 ~ file: PersonalInfo.js:8 ~ PersonalInfo ~ firstName:", firstName)
-  //   console.log("🚀 ~ file: PersonalInfo.js:8 ~ PersonalInfo ~ lastName:", lastName)
   //   setInfo({...info, firstName: firstName, lastName: lastName})
   // }
+
+  // ^ The following work if cv.personalInfo.firstName already exists, even if set to ''
   // const [info, setInfo] = useState({
   //   firstName: cv.personalInfo.firstName,
   //   lastName: cv.personalInfo.lastName,
   // });
 
-  // ^ Use the operational chaning operator.  It enables you to read the value of a property 
-  // ^ located deep within a chain of connected objects without having to check that a child 
+  // ^ Use the operational chaning operator.  It enables you to read the value of a property
+  // ^ located deep within a chain of connected objects without having to check that a child
   // ^ actually exists deep in the object or not.  It returns `undefined` if error.
-  
+
   // ^ React does not like the value of an input to change from `undefined` to defined
   // ^ therefore, I need to change `undefinded` to ''
+  // const [info, setInfo] = useState({
+  //   firstName:
+  //     cv.personalInfo?.firstName === undefined ? '' : cv.personalInfo.firstName,
+  //   lastName:
+  //     cv.personalInfo?.lastName === undefined ? '' : cv.personalInfo.lastName,
+  // });
+  // ^ A cleaner version of the above
+  // undefined || '' = ''
+  // 'hello' || '' = 'hello'
   const [info, setInfo] = useState({
-    firstName:
-      cv.personalInfo?.firstName === undefined ? '' : cv.personalInfo.firstName,
-    lastName:
-      cv.personalInfo?.lastName === undefined ? '' : cv.personalInfo.lastName,
+    firstName: cv.personalInfo?.firstName || '',
+    lastName: cv.personalInfo?.lastName || '',
   });
 
   function handleChange(e, attr) {
